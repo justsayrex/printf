a1d3d6d4163172fda580c84a1a833c187916d811
justsayrex <justsayrex@gmail.com>
2022-07-13 02:34:14 -0700

    printf

--- modifiers.c
+++ modifiers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 unsigned int print_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
+unsigned char flags, int wid);
 unsigned int print_string_width(buffer_t *output,
-		unsigned char flags, int wid, int prec, int size);
+unsigned char flags, int wid, int prec, int size);
 unsigned int print_neg_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
+unsigned char flags, int wid);
 
 /**
  * print_width - Stores leading spaces to a buffer for a width modifier.
@@ -17,19 +17,17 @@ unsigned int print_neg_width(buffer_t *output, unsigned int printed,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int print_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid)
+unsigned char flags, int wid)
 {
-	unsigned int ret = 0;
-	char width = ' ';
-
-	if (NEG_FLAG == 0)
-	{
-		wid -= (prec == -1) ? size : prec;
-		for (; wid > 0; wid--)
-			ret += _memcpy(output, &width, 1);
-	}
-
-	return (ret);
+unsigned int ret = 0;
+char width = ' ';
+if (NEG_FLAG == 0)
+{
+wid -= (prec == -1) ? size : prec;
+for (; wid > 0; wid--)
+ret += _memcpy(output, &width, 1);
+}
+return (ret);
 }
 
 /**
@@ -43,16 +41,14 @@ unsigned int print_width(buffer_t *output, unsigned int printed,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int print_neg_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid)
+unsigned char flags, int wid)
 {
-	unsigned int ret = 0;
-	char width = ' ';
-
-	if (NEG_FLAG == 1)
-	{
-		for (wid -= printed; wid > 0; wid--)
-			ret += _memcpy(output, &width, 1);
-	}
-
-	return (ret);
+unsigned int ret = 0;
+char width = ' ';
+if (NEG_FLAG == 1)
+{
+for (wid -= printed; wid > 0; wid--)
+ret += _memcpy(output, &width, 1);
+}
+return (ret);
 }
